5a34096ce4e07da9a91ac27c0100623e541ebcb6
Rafael Sevilla <rsevilla@virtela.net>
2017-04-21 15:22:49 +0800

    base code for file opening

--- lib/rstyx/client.rb
+++ lib/rstyx/client.rb
@@ -47,6 +47,9 @@ module RStyx
       ##
       # Used FIDs for the connection
       attr_accessor :usedfids
+      ##
+      # Root FID
+      attr_accessor :rootfid
 
       def post_init
         # Initial message buffer
@@ -59,6 +62,7 @@ module RStyx
         @rpendingclunks = Hash.new
         @uname = ENV['USER']
         @aname = ""
+        @rootfid = nil
         # Begin handshaking process with remote server.
         # 1. Send a Tversion message and check the response from the
         #    remote Styx server.
@@ -163,8 +167,7 @@ module RStyx
         end
  
         tmsg.response = styxmsg
-        case styxmsg.class
-        when Message::Rflush
+        if styxmsg.is_a?(Message::Rflush)
           # If we flushed a message by sending a Tflush ourselves
           # then we should also fail the oldtag message and send
           # the Rflush as its response.
@@ -177,7 +180,7 @@ module RStyx
             # Fail the transmitted message if it was not a Tflush
             tmsg.fail("peer flushed message")
           end
-        when Message::Rerror
+        elsif styxmsg.is_a?(Message::Rerror)
           tmsg.fail(styxmsg.ename)
         else
           tmsg.succeed
@@ -224,16 +227,14 @@ module RStyx
             # An error here is most likely a no such fid error. Return the
             # fid manually in this case.
             return_fid(c.fid)
-            cdf.succeed if @usedfids.length == 0
           end
           c.callback do
             cdf.succeed if @usedfids.length == 0
           end
         end
-        cdf.succeed if @usedfids.length == 0
         cdf.callback do
           # flush all outstanding messages before final disconnect
-          @sentmessages.keys.each do |tag|
+          @sentmessages.keys.clone.each do |tag|
             tf = send_message(Message::Tflush.new(:oldtag => tag))
             tf.callback do
               if @sentmessages.length == 0
@@ -317,307 +318,39 @@ module RStyx
           end
         end
 
-        fp = file.open(numeric_mode, perm, create, &block)
-        if append
-          fp.callback do
-            fp.seek(0, 2)
-          end
-        end
+        fp = file.aopen(numeric_mode, perm, create)
+#        if append
+#          fp.callback do
+#            fp.seek(0, 2)
+#          end
+#        end
         return(fp)
       end
     end                         # module StyxClient
 
-    class Connection
-      attr_accessor :usedfids, :pendingclunks, :umask
-      attr_reader :connectstate, :msize, :version
-      attr_reader :rootdirectory, :rootfid, :authenticator
-
-      def initialize(auth=DummyAuthenticator.new)
-        @usedfids = []
-        @pendingclunks = {}
-        @rpendingclunks = {}
-        @conn = nil
-        @rootfid = nil
-        @eventthread = nil
-        @authenticator = auth
-        @clunklock = Mutex.new
-        @umask = ::File.umask
-        @peerauth = nil
-      end
-
-      ##
-      # Get a new free FID.
-      #
-      def get_free_fid
-        found = false
-        val = nil
-        0.upto(MAX_FID) do |i|
-          unless @usedfids.include?(i)
-            val = i
-            break
-          end
-        end
-
-        if val.nil?
-          raise StyxException.new("No more free fids")
-        end
-        @usedfids << val
-        return(val)
-      end
-
-      ##
-      # Returns a fid after we're done using it.
-      #
-      def return_fid(fid)
-        @usedfids.delete(fid)
-      end
-
-      protected
-
-      ##
-      # This method is used to prepare the connection, and should be
-      # defined by subclasses.
-      #
-      def prepare_connection
-        raise StyxException.new("No prepare_connection method defined")
-      end
-
-      public
-
-      ##
-      # Connect to a remote Styx server.  If a block is passed, yield
-      # self to the block and then do a disconnect when the block
-      # finishes.
-      #
-      def connect(&block)
-        prepare_connection()
-        uname = ENV['USER']
-        aname = ""
-
-        # Set the authenticator object
-        @authenticator.connection = @conn
-        @conn.auth = @authenticator
-        @authenticator.authenticate
-
-        # Connection has been established.  Begin the handshaking process
-        # with the remote server.
-        #
-        # 1. Send a Tversion message and check the response from the
-        #    remote Styx server.
-        #
-        rver = send_message(Message::Tversion.new(:msize => 8216,
-                                                  :version => "9P2000"))
-        if (rver.version != "9P2000")
-          raise StyxException.new("Server uses unsupported Styx version #{rver.version}")
-        end
-        @msize = rver.msize
-        @version = rver.version
-        rfid = nil
-        # 2. Attach to the remote server
-        if @auth.nil? || @authenticator.is_a?(Keyring::Authinfo)
-          # unauthenticated connection
-          rfid = get_free_fid
-          rattach = send_message(Message::Tattach.new(:fid => rfid,
-                                                      :afid => NOFID,
-                                                      :uname => uname,
-                                                      :aname => aname))
-        else
-          #
-          # 3. Perform authentication based on the passed authenticator
-          #    object.
-          #
-          # If we have an authenticator object, we call its authenticator
-          # method.
-          #
-          rfid = @auth.authenticate
-        end
-
-        # If we get here, we're connected, and rfid represents the root
-        # fid of the connection
-        @rootfid = rfid
-
-        if block_given?
-          begin
-            yield self
-          ensure
-            self.disconnect
-          end
-        else
-          return(self)
-        end
-      end
-
-      ##
-      # Disconnect from the remote server.
-      #
-      def disconnect
-        # Clunk all outstanding fids in reverse order so the root fid
-        # gets clunked last.
-        while (@usedfids.length > 0)
-          begin
-            rclunk = tclunk(@usedfids[-1], true)
-          rescue
-            # An error is most likely a no such fid error.  Return the fid
-            # manually in this case.
-            return_fid(@usedfids[-1])
-          end
-        end
-
-        @conn.disconnect
-        @eventthread.kill
-      end
-
-      ##
-      # Send a message, and return the response.  Delegates to
-      # @conn#send_message.  Do not use this method to send Tclunk
-      # messages!
-      #
-      def send_message(msg, timeout=0)
-        @conn.send_message(msg, timeout)
-      end
-
-      ##
-      # Fire and forget a Tclunk for some fid.  When the Rclunk is
-      # received, return the fid.  USE THIS METHOD, AND THIS METHOD
-      # ONLY, to send Tclunk messages.
-      #
-      def tclunk(fid, sync=false)
-        if @rpendingclunks.has_key?(fid)
-          return
-        end
-        q = nil
-        if sync
-          q = Queue.new
-        end
-        tag = @conn.send_message_async(Message::Tclunk.new(:fid => fid)) do |tx,rx|
-          # Test whether the response is an Rclunk.
-          if rx.class != Message::Rclunk
-            # this is an error condition, but it will only get reported
-            # if Thread.abort_on_exception is set to true, or if
-            # the tclunk is synchronous
-            exc = StyxException.new("#{tx.to_s} received #{rx.to_s}")
-            if sync
-              q << exc
-            else
-              raise exc
-            end
-          end
-          # return the FID
-          fid = @pendingclunks.delete(tag)
-          @rpendingclunks.delete(fid)
-          return_fid(fid)
-          if sync
-            q << fid
-          end
-        end
-        @pendingclunks[tag] = fid
-        @rpendingclunks[fid] = tag
-        if sync
-          res = q.shift
-          if res.class == StyxException
-            raise res
-          end
-        end
-      end
-
-      ##
-      # Open a file on the remote server, throwing a StyxException if the
-      # file can't be found or opened in a given mode.
-      #
-      # +path+:: The path of the file relative to the server root.
-      # +mode+:: Integer representing the mode, or one of "r", "r+",
-      #          "w", "w+", "a", "a+", "e" as aliases
-      # +return+:: A File object representing the opened file, or
-      #          possibly a Directory object if the file was a directory.
-      #
-      # If a block is passed, it will yield the file object to the block
-      # and close the file when the block finishes (actually it will pass
-      # the block on to the StyxFile#open method, which does just that).
-      #
-      def open(path, mode="r", perm=0666, &block)
-        file = File.new(self, path)
-
-        append = false
-        create = false
-        numeric_mode = nil
-        if mode.is_a?(Integer)
-          numeric_mode = mode
-        else
-          case mode.to_s
-          when "r"
-            numeric_mode = OREAD
-          when "r+"
-            numeric_mode = ORDWR
-          when "w"
-            numeric_mode = OTRUNC | OWRITE
-            create = true
-          when "w+"
-            numeric_mode = OTRUNC | ORDWR
-            create = true
-          when "a"
-            numeric_mode = OWRITE
-            append = true
-            create = true
-          when "a+"
-            numeric_mode = ORDWR
-            append = true
-            create = true
-          when "e"
-            numeric_mode = OEXEC
-          else
-            raise StyxException.new("invalid access mode #{mode}")
-          end
-        end
-
-        fp = file.open(numeric_mode, perm, create, &block)
-        if append
-          fp.seek(0, 2)
-        end
-        return(fp)
-      end
-
-    end                         # class Connection
-
     ##
-    # TCP connection subclass.
-    #
-    class TCPConnection < Connection
-      def initialize(host, port, auth=nil)
-        @host = host
-        @port = port
-        super(auth)
+    # Connect to a Styx server. The parameter is a connection descriptor of
+    # the form proto!host!port.
+    def self.connect(server, &block)
+      proto,host,port = server.split("!")
+      case proto
+      when 'tcp'
+        return(EventMachine.connect(host, port, StyxClient, &block))
+      else
+        raise StyxException, "unknown protocol #{proto}"
       end
-
-      protected
-
-      ##
-      # Prepare a TCP connection to the Styx server
-      #
-      def prepare_connection
-        queue = Queue.new
-        @eventthread = Thread.new do
-          EventMachine::run do
-            queue << EventMachine::connect(@host, @port, StyxClient)
-          end
-        end
-
-        @conn = queue.shift
-      end
-
-      public
-
-    end                         # class TCPConnection
+    end
 
     ##
     # A Styx client's view of a file.  This class should probably
-    # never be directly instantiated, but only via Connection#open.
+    # never be directly instantiated, but only via StyxClient#open.
     # The buffering algorithm in use here is somewhat based on the
     # Buffering mix-in module in the Ruby OpenSSL module written by
     # Goto Yuuzou, but modified a bit to provide for offset
-    # handling.  Note that this class isn't thread-safe.
+    # handling.
     #
     class File
-      include Enumerable
+      include Enumerable, EventMachine::Deferrable
 
       attr_reader :conn, :path
       attr_accessor :mode, :fid, :qid, :iounit, :sync
@@ -638,9 +371,7 @@ module RStyx
       end
 
       ##
-      # Open the file on the server.  If a block is passed to this method
-      # it will pass the file to the block and close the file automatically
-      # when the block terminates.
+      # Open the file on the server asynchronously.
       #
       # This follows more or less the same semantics as sys-open(2)
       # on Inferno, performing an open with truncate, when a file
@@ -653,102 +384,119 @@ module RStyx
       # +mode+:: Integer representing the mode - see the constants in common.rb
       # +perm+:: Permissions of the file (only used on open/create)
       # +create+:: should we create the file if it doesn't exist?
+      # Returns:: Self. Attach any callbacks/errbacks to it.
       #
-      def open(mode, perm, create, &block)
-        dfid = @conn.get_free_fid
+      def aopen(mode, perm, create)
+        dfid = @conn.newfid
         basename = ::File.basename(@path)
         dirname = ::File.dirname(@path)
-        # Walk to the dirname first
         twalk = Message::Twalk.new(:fid => @conn.rootfid, :newfid => dfid)
         twalk.path = dirname
-        rwalk = @conn.send_message(twalk)
-        # if the rwalk has some other length than the number of path
-        # elements in the original twalk, we have failed.
-        if rwalk.qids.length != twalk.wnames.length
-          raise StyxException.new(("#{path} no such file or directory"))
-        end
-        ropen = fid = nil
-        begin
+        @conn.send_message(twalk)
+        twalk.errback { |err| self.fail(err) }
+        twalk.callback do
+          rwalk = twalk.response
+          # if the rwalk has some other length than the number of path
+          # elements in the original twalk, some element of the path doesn't
+          # exist and the file can't be opened.
+          if rwalk.qids.length != twalk.wnames.length
+            self.fail("#{path} no such file or directory")
+            next
+          end
+          ropen = fid = nil
           # Next, walk to the basename from there, using a new fid
-          fid = @conn.get_free_fid
+          fid = @conn.newfid
           twalk = Message::Twalk.new(:fid => dfid, :newfid => fid)
           twalk.path = basename
-          rwalk = @conn.send_message(twalk)
-          # Do a Topen if this succeeds
-          open_msg = Message::Topen.new(:fid => fid, :mode => mode)
-          ropen = @conn.send_message(open_msg)
-          @conn.tclunk(dfid)
-        rescue Exception => e
-          # If we are being directed to create the file if it doesn't
-          # already exist, send a Tcreate message, and use the response
-          # for it as the ropen (the two classes respond to exactly the
-          # same messages--ah the wonders of duck typing!).  If not,
-          # or if that fails, this should propagate an exception upwards.
-          if create
-            # Alter the submitted permissions mask according to
-            # the connection's umask
-            perm &= ~(@conn.umask)
-            create_msg = Message::Tcreate.new(:fid => dfid, :name => basename,
-                                              :perm => perm, :mode => mode)
-            ropen = @conn.send_message(create_msg)
-            fid = dfid
-          else
-            raise e
+          @conn.send_message(twalk)
+          twalk.errback do |err|
+            # XXX -- file creation
+            self.fail(err)
           end
-        end
+          twalk.callback do
+            topen = @conn.send_message(Message::Topen.new(:fid => fid, :mode => mode))
+            topen.callback do
+              ropen = topen.response
+              cl=@conn.tclunk(dfid)
+              cl.callback do
+                # If we get here, we were able to successfully open or create
+                # the file.
+                @mode = mode
+                @fid = fid
+                @qid = ropen.qid
+                @iounit = ropen.iounit
+                # XXX: Determine if the file is actually a directory.  Such a
+                # file would have its Qid.qtype high bit set to 1. In this
+                # case, instead of passing back self, we should pass a
+                # Directory object.
+                self.succeed(self)
+              end
+              cl.errback { |err| self.fail(err) }
+            end
 
-        # If we get here, we were able to successfully open or create
-        # the file.
-        @mode = mode
-        @fid = fid
-        @qid = ropen.qid
-        @iounit = ropen.iounit
-        # Determine if the file is actually a directory.  Such a file
-        # would have its Qid.qtype high bit set to 1.  In this case, instead
-        # of returning self, we return self wrapped in a Directory object.
-        retval = self
-        if twalk.wnames.length == 0 || (rwalk.qids[-1].qtype & 0x80 != 0)
-          retval = Directory.new(self)
-        end
-        if block_given?
-          begin
-            yield retval
-          ensure
-            retval.close
+            topen.errback do |err|
+              # XXX -- file creation
+              self.fail(err)
+            end
           end
-        else
-          return(retval)
         end
+        return(self)
       end
 
       ##
       # Read the Stat information for the file.
       #
-      # returns: the RStyx::Message::Stat instance corresponding to
-      #          the file
+      # returns: self, callback returns the RStyx::Message::Stat instance
+      # corresponding to the file.
       #
-      def stat
-        rstat = @conn.send_message(Message::Tstat.new(:fid => @fid))
-        return(rstat.stat)
+      def astat
+        return(@conn.send_message(Message::Tstat.new(:fid => @fid)))
       end
 
       ##
-      # Close the file.  This will flush all unwritten buffered data
-      # if any.
+      # Close the file.  XXX: This should flush all unwritten buffered data
+      # if any. Uses self as a deferrable.
       #
-      def close
-        flush rescue nil
+      def aclose
         if @fid >= 0
           # Clunk the fid
-          @conn.tclunk(@fid)
+          clunk = @conn.tclunk(@fid)
+          clunk.callback do
+            @fid = -1
+            @iounit = 0
+            @mode = -1
+            self.succeed(self)
+          end
+          clunk.errback do |err|
+            @fid = -1
+            @iounit = 0
+            @mode = -1
+            self.fail(err)
+          end
         end
-        @fid = -1
-        @iounit = 0
-        @mode = -1
+        self.succeed(self)
       end
 
       def closed?
         return(@mode < 0)
+      end
+
+      ##
+      # Read at most +size+ bytes or up to iounit. Returns a new
+      # Deferrable, which succeeds when the Rread is received, or
+      # fails on an Rerror. Will return zero size responses on
+      # end of file. Should probably not be used directly.
+      def _asysread(size, offset)
+        df = EventMachine::DefaultDeferrable.new
+        if size > @iounit
+          size = @iounit
+        end
+        tr = @conn.send_message(Message::Tread.new(:fid => @fid,
+                                                   :offset => offset,
+                                                   :count => size))
+        tr.callback { df.succeed(tr.response.data) }
+        tr.errback { |err| df.fail(err) }
+        return(df)
       end
 
       private
